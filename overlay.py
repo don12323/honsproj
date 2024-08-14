425e99fec969eb7683c5a9b5f57627e4737f8ad0
Don Wijesinghe <imesh1245@gmail.com>
2024-08-15 02:41:56 +0800

    added numbers to HG markers

--- overlay.py
+++ overlay.py
@@ -21,12 +21,12 @@ def main(infrared_fits, radio_fits, rms_r, rms_c, contour_fits, coords_file):
     # Load the radio image
     with fits.open(radio_fits) as radio_hdul:
         radio_data = radio_hdul[0].data
-        radio_wcs = WCS(radio_hdul[0].header)
+        radio_wcs = WCS(radio_hdul[0].header, naxis=2)
         radio_header = radio_hdul[0].header  # Store the header for later use
     # Load the contour image
     with fits.open(contour_fits) as contour_hdul:
         contour_data = contour_hdul[0].data
-        contour_wcs = WCS(contour_hdul[0].header,naxis=2)
+        contour_wcs = WCS(contour_hdul[0].header, naxis=2)
         contour_header = contour_hdul[0].header
 
     # Load the coordinates from the text file
@@ -53,14 +53,14 @@ def main(infrared_fits, radio_fits, rms_r, rms_c, contour_fits, coords_file):
     # Plot the reprojected infrared image
     ax.imshow(ir_reproj, cmap='gray', origin='lower',
               vmin=np.percentile(ir_reproj, 5),
-              vmax=np.percentile(ir_reproj, 99.5))
+              vmax=np.percentile(ir_reproj, 99.7))
 
     # Set axis labels
     ax.set_xlabel('R.A. (J2000)')
     ax.set_ylabel('Dec. (J2000)')
 
     # Overlay the masked radio image in mJy
-    ax.imshow(radio_masked * 1e3, cmap='inferno', origin='lower', alpha=0.6)
+    ax.imshow(radio_masked * 1e3, cmap='inferno', origin='lower', alpha=0.4)
 
     # Customize tick parameters
     ax.tick_params(direction='in', colors='white')
@@ -68,18 +68,19 @@ def main(infrared_fits, radio_fits, rms_r, rms_c, contour_fits, coords_file):
     ax.tick_params(axis='y', which='both', labelcolor='black')
 
     # Plot radio contours
-    contour_lvls = np.array([-3, 6, 9, 12, 15, 18, 21, 24, 27, 30]) * rms_c
-    ax.contour(cont_reproj, levels=contour_lvls, cmap='YlOrRd', linewidths=0.8, alpha=0.65,transform=ax.get_transform(radio_wcs))
-    ax.contour(radio_data, levels=contour_lvls, cmap='YlOrRd', linewidths=0.8, alpha=0.65,transform=ax.get_transform(radio_wcs))
+    contour_lvls = np.array([3, 6, 9, 12, 15, 18, 21, 24]) * rms_c
+    ax.contour(cont_reproj, levels=contour_lvls, cmap='YlOrRd', linewidths=0.8, alpha=0.7,transform=ax.get_transform(radio_wcs))
+    ax.contour(radio_data, levels=[3 * rms_r], cmap='inferno', linewidths=1, alpha=0.6,transform=ax.get_transform(radio_wcs))
     
     # Mark possible host galaxy positions with 'X' and number them
-    for c in host_coords:
-        ax.plot(c.ra.deg, c.dec.deg,marker='x', color='cyan',transform=ax.get_transform('fk5'), markersize=10)
-
-
+    for i, c in enumerate(host_coords,start=1):
+        ax.plot(c.ra.deg, c.dec.deg,marker='x', color='cyan',transform=ax.get_transform('fk5'), markersize=8)
+        if len(host_coords) > 1:
+            ax.text(c.ra.deg, c.dec.deg, f'   {i}', color='cyan', transform=ax.get_transform('fk5'), fontsize=8, ha='left', va='top')
 
     # Add the synthesized beam ellipse
-    wcsaxes.add_beam(ax, header=radio_header)
+    wcsaxes.add_beam(ax, header=contour_header,alpha=0.6)
+    wcsaxes.add_beam(ax,header=radio_header,alpha=0.6, color='orange')
 
     # Add the radio image color bar
     cbar = fig.colorbar(ax.images[-1], ax=ax, shrink=1, pad=0.04)
